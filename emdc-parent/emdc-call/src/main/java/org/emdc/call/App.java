405fc7da19bb85d4cda1dfa66ada52dc8327ab74
zeroflag <zeroflag@126.com>
2016-04-14 18:42:14 +0800

    emdc demo

---
+++ emdc-parent/emdc-call/src/main/java/org/emdc/call/App.java
@@ -0,0 +1,13 @@
+package org.emdc.call;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ emdc-parent/emdc-web/src/main/java/org/emdc/web/demo/Example.java
@@ -0,0 +1,19 @@
+package org.emdc.web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    
+	@RequestMapping("/")
+	String home(){
+		return "Greetings from Spring Boot!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Example.class, args);
+	}
+}
